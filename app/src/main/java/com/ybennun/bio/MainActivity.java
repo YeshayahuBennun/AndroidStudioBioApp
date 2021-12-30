754de00b164ce3b7b3436055229bff1c8cec26eb
YeshayahuBennun <ybennun@yahoo.com>
2021-12-30 20:55:19 +0200

    commit#6 - BioApp - Improving with Data Binding Object

--- app/src/main/java/com/ybennun/bio/MainActivity.java
+++ app/src/main/java/com/ybennun/bio/MainActivity.java
@@ -1,31 +1,31 @@
 package com.ybennun.bio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.TextView;
+
+
+import com.ybennun.bio.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText enterHobbies;
-    private TextView hobbies;
-
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        enterHobbies = findViewById(R.id.enter_hobbies);
 
-        hobbies = findViewById(R.id.hobbies_text);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.doneButton.setOnClickListener(this::addHobbies);
     }
 
     public void addHobbies(View view) {
-        hobbies.setText(String.format("Hobbies: %s", enterHobbies.getText().toString().trim()));
-        hobbies.setVisibility(View.VISIBLE);
+        binding.hobbiesText.setText(String.format("Hobbies: %s", binding.enterHobbies.getText().toString().trim()));
+        binding.invalidateAll();
+        binding.hobbiesText.setVisibility(View.VISIBLE);
 
         //hide keyboard
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
